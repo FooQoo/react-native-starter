ffbfdc7cac240c713ed881368194a4f4f81af5e5
FooQoo <horseshoes.buzz.17@gmail.com>
2022-08-13 23:52:22 +0900

    fix tab2

--- src/screens/TabTwoScreen.tsx
+++ src/screens/TabTwoScreen.tsx
@@ -1,13 +1,17 @@
 import { Text } from 'native-base';
-import { View } from 'react-native';
 import { StyleSheet } from 'react-native';
 import EditScreenInfo from 'src/components/EditScreenInfo';
+import { View } from 'src/components/Themed';
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
     </View>
   );
